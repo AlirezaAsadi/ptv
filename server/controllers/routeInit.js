92339a660149ba5afd2059ee0ac8f8301e8daf50
abhiabrol <abhi.abrol34@gmail.com>
2016-08-29 20:31:19 +1000

    Add files via upload

--- server/controllers/routeInit.js
+++ server/controllers/routeInit.js
@@ -27,6 +27,12 @@ module.exports = function (app) {
 
     app.get('/about', function (req, res) {
         res.render('about');
+    
+    app.get('/contactUs', function (req, res) {
+        res.render('contact');
+
+    app.get('/myki', function (req, res) {
+        res.render('myki');    
     });
 
 };
